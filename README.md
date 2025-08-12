aacd3280a4372d039bc7cedff3e97fffb681fb04
Felix Rodemund <586212+reflix@users.noreply.github.com>
2025-08-12 10:08:57 +0200

    Add TTL-Support

--- README.md
+++ README.md
@@ -1,30 +1,25 @@
 ```
-rupnpc 0.1.1
-Simple UPnP discoverer written in rust.
+Simple UPnP discoverer written in rust
 
-USAGE:
-    rupnpc [OPTIONS]
+Usage: rupnpc [OPTIONS]
 
-FLAGS:
-    -h, --help       Prints help information
-    -V, --version    Prints version information
-    -w, --warn       Show warnings on erroneous responses
-
-OPTIONS:
-    -d, --duration <duration>              Scan duration in seconds [default: 3]
-    -f, --format <format>                  Set output format. Available format strings are:
-                                               - name
-                                               - manufacturer
-                                               - model_name
-                                               - udn
-                                               - upc
-                                               - serial
-                                               - manufacturer_url
-                                               - model_description
-                                               - model_url
-                                               - model_number
-                                               - url
-                                               - device_type
-                                           To print name and url for each discovered item pass -f "{name} {url}".
-    -s, --search-target <search-target>
+Options:
+  -s, --search-target <SEARCH_TARGET>  
+  -d, --duration <DURATION>            Scan duration in seconds [default: 3]
+  -w, --warn                           Show warnings on erroneous responses
+  -f, --format <FORMAT>                Set output format. Available format strings are:
+                                           - name
+                                           - manufacturer
+                                           - model_name
+                                           - udn
+                                           - upc
+                                           - serial
+                                           - manufacturer_url
+                                           - model_description
+                                           - model_url
+                                           - model_number
+                                           - url
+                                           - device_type
+                                       To print name and url for each discovered item pass -f "{name} {url}".
+  -h, --help                           Print help
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use futures::TryStreamExt;
+use rt_format::{FormatArgument, ParsedFormat, Specifier};
+use rupnp::Device;
+use rupnp::http::Uri;
+use rupnp::ssdp::{SearchTarget, URN};
 use std::collections::HashMap;
 use std::fmt::Formatter;
 use std::str::FromStr;
 use std::time::Duration;
-use futures::TryStreamExt;
-use rupnp::ssdp::{SearchTarget, URN};
-use structopt::StructOpt;
-use anyhow::{Context, Result};
-use rt_format::{FormatArgument, ParsedFormat, Specifier};
-use rupnp::Device;
-use rupnp::http::Uri;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "rupnpc", about = "Simple UPnP discoverer written in rust.")]
+#[derive(Debug, Parser)]
+#[command(name = "rupnpc", about = "Simple UPnP discoverer written in rust")]
 struct Args {
-    #[structopt(long, short)]
+    #[arg(long, short)]
     search_target: Option<String>,
 
     /// Scan duration in seconds.
-    #[structopt(long, short, default_value = "3")]
+    #[arg(long, short, default_value = "3")]
     duration: u8,
 
+    /// Time to live
+    #[arg(long, short)]
+    ttl: Option<u32>,
+
     /// Show warnings on erroneous responses
-    #[structopt(long, short)]
+    #[arg(long, short)]
     warn: bool,
 
     /// Set output format. Available format strings are:
@@ -38,18 +42,19 @@ struct Args {
     ///     - url
     ///     - device_type
     /// To print name and url for each discovered item pass -f "{name} {url}".
-    #[structopt(long, short, verbatim_doc_comment)]
+    #[arg(long, short, verbatim_doc_comment)]
     format: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::from_args();
-    println!("warn {}", args.warn);
-    let search_target: SearchTarget = (&args).try_into()?;
-    let devices = rupnp::discover(&search_target, (&args).into()).await?;
+    let args = Args::parse();
+    let search_target = (&args).try_into()?;
+    let duration = Duration::from_secs(args.duration as u64);
+    let devices = rupnp::discover(&search_target, duration, args.ttl).await?;
     pin_utils::pin_mut!(devices);
     let format = args.format.unwrap_or_else(|| "name: {name}, manufacturer: {manufacturer}, model_name: {model_name}, udn: {udn}, url: {url}".into());
+
     let vec = Vec::new();
     let mut erroneous_responses = 0;
     loop {
@@ -88,25 +93,18 @@ impl TryFrom<&Args> for SearchTarget {
     fn try_from(arguments: &Args) -> std::result::Result<Self, Self::Error> {
         match &arguments.search_target {
             None => Ok(SearchTarget::All),
-            Some(value) => {
-                SearchTarget::from_str(value.as_str()).with_context(|| format!("Unable to convert '{value}' to an URN."))
-            }
+            Some(value) => SearchTarget::from_str(value.as_str())
+                .with_context(|| format!("Unable to convert '{value}' to an URN.")),
         }
     }
 }
 
-impl From<&Args> for Duration {
-    fn from(args: &Args) -> Self {
-        Duration::from_secs(args.duration as u64)
-    }
-}
-
 trait NamedArgs {
-    fn to_named_args(&self) -> HashMap<&str, Value>;
+    fn to_named_args(&self) -> HashMap<&str, Value<'_>>;
 }
 
 impl NamedArgs for Device {
-    fn to_named_args(&self) -> HashMap<&str, Value> {
+    fn to_named_args(&self) -> HashMap<&str, Value<'_>> {
         let mut named_args = HashMap::new();
         named_args.insert("name", self.friendly_name().into());
         named_args.insert("manufacturer", self.manufacturer().into());
@@ -133,7 +131,10 @@ enum Value<'a> {
 
 impl<'a> FormatArgument for Value<'a> {
     fn supports_format(&self, specifier: &Specifier) -> bool {
-        matches!(specifier.format, rt_format::Format::Display | rt_format::Format::Debug)
+        matches!(
+            specifier.format,
+            rt_format::Format::Display | rt_format::Format::Debug
+        )
     }
 
     fn fmt_display(&self, f: &mut Formatter) -> std::fmt::Result {
